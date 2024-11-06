164db74f8c438dbf95a94b01abd75e574dd01e42
ChiDuck <chiducocelot@gmail.com>
2024-11-07 01:41:10 +0700

    TheloaiController and View

---
+++ OnlineLibraryManagement/Controllers/TheloaiController.cs
@@ -0,0 +1,72 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using OnlineLibraryManagement.Models;
+using OnlineLibraryManagement.MyModels;
+
+namespace OnlineLibraryManagement.Controllers
+{
+    public class TheloaiController : Controller
+    {
+        QuanLyThuVienContext db = new QuanLyThuVienContext();
+
+        public IActionResult Index()
+        {
+            List<CTheloai> list = db.Theloais.Select(i => CTheloai.chuyenDoi(i)).ToList();
+            return View(list);
+        }
+
+        public IActionResult frmThemTheloai()
+        {
+            return View();
+        }
+
+        public IActionResult themTheloai(CTheloai ctl)
+        {
+            if (ModelState.IsValid)
+            {
+                Theloai tl = CTheloai.chuyenDoi(ctl);
+                db.Theloais.Add(tl);
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+
+            return View("frmThemTheloai");
+        }
+
+        public IActionResult frmXoaTheloai(int id)
+        {
+            Theloai tl = db.Theloais.Find(id);
+            return View(CTheloai.chuyenDoi(tl));
+        }
+
+        public IActionResult xoaTheloai(int id)
+        {
+            try
+            {
+                Theloai tl = db.Theloais.Find(id);
+                db.Theloais.Remove(tl);
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+            catch
+            {
+                ErrorViewModel error = new ErrorViewModel();
+                error.RequestId = "Không thể xóa thể loại này!";
+                return View("Error", error);
+            }
+        }
+
+        public IActionResult frmSuaTheloai(int id)
+        {
+            CTheloai ctl = CTheloai.chuyenDoi(db.Theloais.Find(id));
+            return View(ctl);
+        }
+
+        public IActionResult suaTheloai(CTheloai ctl)
+        {
+            Theloai tl = CTheloai.chuyenDoi(ctl);
+            db.Theloais.Update(tl);
+            db.SaveChanges();
+            return RedirectToAction("Index");
+        }
+    }
+}
---
+++ OnlineLibraryManagement/MyModels/CTheloai.cs
@@ -0,0 +1,33 @@
+﻿using OnlineLibraryManagement.Models;
+using System.ComponentModel.DataAnnotations;
+
+namespace OnlineLibraryManagement.MyModels
+{
+    public class CTheloai
+    {
+        private QuanLyThuVienContext db = new QuanLyThuVienContext();
+
+        [Display(Name = "Mã thể loại")]
+        public int Maloai { get; set; }
+        [Display(Name = "Tên thể loại")]
+        [Required(ErrorMessage = "Tên thể loại không thể bỏ trống")]
+        public string? Tenloai { get; set; }
+
+        public static CTheloai chuyenDoi(Theloai tl)
+        {
+            return new CTheloai
+            {
+                Maloai = tl.Maloai,
+                Tenloai = tl.Tenloai 
+            };
+        } 
+        public static Theloai chuyenDoi(CTheloai ctl)
+        {
+            return new Theloai
+            {
+                Maloai = ctl.Maloai,
+                Tenloai = ctl.Tenloai 
+            };
+        }
+    }
+}
