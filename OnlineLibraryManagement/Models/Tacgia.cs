eac54d14e0ec49bfdd98ab1990ba666841e4cf5f
Đặng Minh Nghĩa <dangminhnghia06122003@gmail.com>
2024-12-19 22:06:23 +0700

    Báo cáo

--- OnlineLibraryManagement/Models/Tacgia.cs
+++ OnlineLibraryManagement/Models/Tacgia.cs
@@ -1,4 +1,6 @@
-﻿namespace OnlineLibraryManagement.Models
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace OnlineLibraryManagement.Models
 {
     public partial class Tacgia
     {
@@ -6,11 +8,15 @@
         {
             Phienbansach = new HashSet<Phienbansach>();
         }
-
+        [Display(Name = "Mã tác giả")]
         public int Matacgia { get; set; }
+        [Display(Name = "Tên tác giả")]
         public string? Tentacgia { get; set; }
+        [Display(Name = "Ngày sinh")]
         public DateTime? Ngaysinh { get; set; }
+        [Display(Name = "Ngày mất")]
         public DateTime? Ngaymat { get; set; }
+        [Display(Name = "Quốc tịch")]
         public string? Quoctich { get; set; }
 
         public virtual ICollection<Phienbansach> Phienbansach { get; set; }
